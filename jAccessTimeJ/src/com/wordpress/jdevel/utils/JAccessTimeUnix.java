4302b42a25a2097e73f33e6bb0e5e750adabc14e
Marek Piechut <Marek Piechut@ptc155>
2011-04-09 00:34:41 +0200

    Initial import

--- jAccessTimeJ/src/com/wordpress/jdevel/utils/JAccessTimeUnix.java
+++ jAccessTimeJ/src/com/wordpress/jdevel/utils/JAccessTimeUnix.java
@@ -4,9 +4,12 @@
  */
 package com.wordpress.jdevel.utils;
 
+import java.io.File;
+import java.util.Date;
+
 /**
  *
- * @author Marek Piechut <mco (at) o2.pl>
+ * @author Marek Piechut
  */
 public class JAccessTimeUnix {
 
@@ -19,12 +22,20 @@ public class JAccessTimeUnix {
      * @param path
      * @return
      */
-    native public static int getLastAccessTime(String path);
+    native private static int getLastAccessTime(String path);
 
+    public static Date getLastAccessTime(File file) {
+        long time = getLastAccessTime(file.getPath());
+        //Last access time is stored in seconds, thus * 1000
+        time *= 1000;
+        Date date = new Date(time);
+        return date;
+    }
+    
     public static void main(String[] args) {
         if (args.length > 0) {
             System.out.println("Last Access Time of: " + args[0]);
-            System.out.println("" + getLastAccessTime(args[0]));
+            System.out.println(getLastAccessTime(new File(args[0])));
         } else {
             System.out.println("Pass file path as parameter");
         }
